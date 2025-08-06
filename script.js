b5b4738d1b1f6c1b79fc38a3992e6dff7a0d5f31
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-06 16:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+function show(){
+	let values = document.querySelectorAll("input");
+
+	let str = "";
+	let arr = ["First Name: ", " Last Name: ", " Phone Number: ", " Email ID: "];
+	for(let i = 0; i < 4; i++){
+		str += arr[i];
+		str+= values[i].value;
+	}
+
+	alert(str);
+}
+
+window.onload = () => {
+      document.getElementById("myForm").onsubmit = show;
+    };
